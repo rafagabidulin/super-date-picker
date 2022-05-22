784fa67c2b8de9fc1ce9cf3bff60bd2756493922
rafagabidulin <rafagabidulin@gmail.com>
2022-05-22 21:35:30 +0300

    added arrays

--- src/components/DatePicker/index.jsx
+++ src/components/DatePicker/index.jsx
@@ -2,12 +2,20 @@ import React from 'react';
 import './index.css'
 
 class DatePicker extends React.Component {
+  static defaultProps = {
+    years: [2010, 2011, 2012, 2013, 2014, 2015, 2016, 2017, 2018, 2019, 2020, 2021, 2022],
+    months: ['Январь', 'Февраль', 'Март', 'Апрель', 'Май', 'Июнь', 'Июль', 'Август', 'Сентябрь', 'Октябрь', 'Ноябрь', 'Декабрь'],
+    weekDays: ['Пн', 'Вт', 'Ср', 'Чт', 'Пт', 'Сб', 'Вс']
+  }
   render() {
+    const { years, months, weekDays } = this.props;
     return (
       <div className="datePicker">
         <header>
           <button>{'<'}</button>
-          <select></select>
+          <select>
+            <option></option>
+          </select>
           <select></select>
           <button>{'>'}</button>
         </header>
