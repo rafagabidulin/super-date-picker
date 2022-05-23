5f6115e5b0954018b43d44756ec90cfe9a1bf1e9
rafagabidulin <rafagabidulin@gmail.com>
2022-05-23 23:09:01 +0300

    some code

--- src/components/DatePicker/calendar.js
+++ src/components/DatePicker/calendar.js
@@ -1,12 +1,45 @@
 const weekDays = 7;
 
-export function getDaysInMonth(date) {
+const daysInMonth = [31, 28, 31, 30, 31, 30, 31, 31, 30, 31, 30, 31];
 
-}
+const monthNames = {
+  January: 0,
+  February: 1,
+  March: 2,
+  April: 3,
+  May: 4,
+  June: 5,
+  July: 6,
+  August: 7,
+  September: 8,
+  October: 9,
+  November: 10,
+  December: 11
+};
+
+export function isLeapYear(year) {
+  return !((year % 4) || (!(year % 100) && (year % 400)));
+};
+
+export function getDaysInMonth(date) {
+  const month = date.getMonth();
+  const year = date.getFullYear();
+
+  if (isLeapYear(year) && month === monthNames.February) {
+    return daysInMonth[month] + 1;
+  } else {
+    return daysInMonth[month];
+  }
+};
 
 export function getDaysOfWeek(date) {
-  
-}
+  const dayOfWeek = date.getDay();
+
+  if (dayOfWeek === 0) {
+    return 6
+  }
+  return dayOfWeek - 1;
+};
 
 export function getMonthData(year, month) {
   const result = [];
@@ -18,18 +51,13 @@ export function getMonthData(year, month) {
   for (let i = 0; i < (daysInMonth + monthStartsOn) / weekDays; i += 1) {
     result[i] = [];
     for (let j = 0; j < weekDays; j += 1) {
-      result[i][j] = new Date(year, month, day += 1)
+      if ((i === 0 && j < monthStartsOn) || day > daysInMonth) {
+        result[i][j] = undefined;
+      } else {
+        result[i][j] = new Date(year, month, day += 1);
+      }
     }
-  }
-
-
-  // return [
-  //   [new Date(), new Date(), new Date(), new Date(), new Date(), new Date(), new Date()],
-  //   [new Date(), new Date(), new Date(), new Date(), new Date(), new Date(), new Date()],
-  //   [new Date(), new Date(), new Date(), new Date(), new Date(), new Date(), new Date()],
-  //   [new Date(), new Date(), new Date(), new Date(), new Date(), new Date(), new Date()],
-  //   [new Date(), new Date(), new Date(), new Date(), new Date(), new Date(), new Date()]
-  // ];
+  };
 
   return result;
 }
