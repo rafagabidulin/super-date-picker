6a352681ad20cc981195dd30d954cb637a5633de
rafagabidulin <rafagabidulin@gmail.com>
2022-05-24 23:08:17 +0300

    changed App to class component and added state

--- src/App.jsx
+++ src/App.jsx
@@ -1,11 +1,22 @@
+import React from 'react';
 import DatePicker from './components/DatePicker';
 
-function App() {
-  return (
-    <div>
-      <DatePicker />
-    </div>
-  );
+class App extends React.Component {
+  state = {
+    date: null
+  };
+
+  handleDateChange = date => this.setState({ date })
+  render() {
+    const { date } = this.state;
+
+    return (
+      <div>
+        {date && <p>Выбранная дата: {date.toLocaleDateString()}</p>}
+        <DatePicker onChange={this.handleDateChange} />
+      </div>
+    );
+  }
 }
 
 export default App;
